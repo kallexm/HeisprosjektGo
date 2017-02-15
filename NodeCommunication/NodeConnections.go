afe13940f99e729cdb1387b0a344b8b524d08e08
larserikjunge <ingvild-f@hotmail.com>
2017-02-15 10:56:58 +0100

    competed several of the functions

--- NodeCommunication/NodeConnections.go
+++ NodeCommunication/NodeConnections.go
@@ -25,22 +25,31 @@ func init(){
 }
 
 
-func Get_master_node_IP_and_port() (string, string){
+func Get_master_node_IP_and_port() (string, string, error){
+	var err error
 	for i := 0; i < len(connectionTable); i++ {
 		if connectionTable[i].isMasterNode == true {
-
+			return connectionTable[i].ip, connectionTable[i].TCPport, err
 		}
 	}
 }
 
 func Get_elevator_node_IP_and_port(n int){
-
+	return connectionTable[n].ip, connectionTable[n].TCPport, err
 }
 
 func Add_new_elevator_node(ip string, TCPport string, nodeIdentity uint8){
-
+	singleConnection 
+	connectionTable = append(connectionTable, singleConnection{ip: ip, TCPport: TCPport, nodeIdentity: nodeIndentity})
+	return err
 } 
 
 func Set_master_node(n int){
-	
+	for i := 0; i < len(connectionTable); i++ {
+		if connectionTable[i].isMasterNode == true {
+			connectionTable[i].isMasterNode = false
+		}
+	}
+	connectionTable[n].isMasterNode = true
+	return err
 }
